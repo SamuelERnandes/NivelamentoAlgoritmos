97754619db1c6e3b15d258252959b72cb3ab503f
SamuelERnandes <samuelernandes_2012@hotmail.com>
2024-08-16 21:52:10 -0300

    inserindo calculo de horas- minutos-segundos com base nos segundos

--- index.java
+++ index.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner prompt = new Scanner(System.in);
+        System.out.print("Informe as horas: ");
+        int horas = prompt.nextInt();
+        System.out.print("Informe os minutos: ");
+        int minutos = prompt.nextInt();
+        System.out.print("Informe os segundos: ");
+        int segundos = prompt.nextInt();
+        int resultado = (horas*60*60) + (minutos*60) + segundos;
+        System.out.println("Resultado em Segundos: "+resultado);
+
+
+
     }
 }
